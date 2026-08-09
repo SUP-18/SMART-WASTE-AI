eafb9e0f8a85223902c1a1c33c2bcd65dee0faf0
SUP-18 <chakkanisuprajaraj18@gmail.com>
2026-08-09 19:51:33 +0530

    Allow images from Unsplash and Supabase Storage in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
     buildActivity: false,
     buildActivityPosition: 'bottom-right',
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dkiunmuwdkajxsvowuvy.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
